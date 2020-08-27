06c29d7cece3c051cdbd7c499dd2d9ab61896d7c
Danilo N Kawanishi <danilonck@Danilos-MacBook-Pro.local>
2020-08-26 23:46:07 -0300

    Done

--- desafio-fundamentos-nodejs/src/repositories/TransactionsRepository.ts
+++ desafio-fundamentos-nodejs/src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (accumulator: number, currentValue: number) =>
+      accumulator + currentValue;
+
+    const incomesTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income').map((transaction) => {
+        return transaction.value;
+      });
+    const outcomesTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome').map((transaction) => {
+        return transaction.value;
+      });
+
+    let income = 0;
+    let outcome = 0;
+
+    if (incomesTransactions.length > 0) {
+      income = incomesTransactions.reduce(reducer);
+    }
+
+    if (outcomesTransactions.length > 0) {
+      outcome = outcomesTransactions.reduce(reducer);
+    }
+
+    return {
+      income,
+      outcome,
+      total: income - outcome
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
